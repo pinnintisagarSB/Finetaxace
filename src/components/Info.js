b210e53d7cbafdf808700a2c0a6d27c3994761ca
SagarSB <pinnintisagar546@gmail.com>
2021-12-07 22:43:35 +0530

    commit on main

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,6 +1,6 @@
 const Info = [{
   title: "Business Registrations",
-  options: ["JHARKHAND VAT REGISTRATION","JHARKHAND PROFESSIONAL TAX REGISTRATION","TRUST OR NGO FORMATION","12A & 80G REGISTRATION OF NGOs","SECTION 8 COMPANY FORMATION","COMPANY INCORPORATION","OTHER BUSINESS"],
+  options: ["JHARKHAND VAT REGISTRATION","JHARKHAND PROFESSIONAL TAX REGISTRATION","TRUST OR NGO FORMATION","12A & 80G REGISTRATION OF NGOs","SECTION 8 COMPANY FORMATION","COMPANY INCORPORATION","OTHER BUSINESSES"],
   docs:[" Minimum 2 directors and 2 shareholders for Private Limited Company"," Name of Company with meaning of Name",
         "Consent to act as directors."," DIN apply for each directors.","KYC documents of each director and shareholders.",
         "PAN & Aadhar of directors (self attested)","Voter/passport/Driving License (self attested)","Bank statement/ passbook with latest 2 months transaction (self attested)",
@@ -9,7 +9,25 @@ const Info = [{
     ],
   work:["DIN apply for directors","Company registration with ROC","PAN of company apply","TAN of company ( for TDS)","GST registration",
        "PF & ESI registration","Udyam Registration ","Establishment of Office Registration"],
-  price:"13999",
+  price:"Rs.13999",
   id:1,     
+},
+{
+  title: "GST FILLING",
+  options: ["GST REGISTRATION","TIME LIMIT 5-7 working days"],
+  docs :["PAN of Individual/Firm/Company","Aadhar","Email and mobile no","Firm name","Bank statement","Electricity bill"," Rent agreement",
+        "Products or services details","Authorisation letter (in case of Firm or LLP)","Board resolution (in case of company)","PAN and Aadhar of All partners/Directors"],
+  work:["GST Registration and other GST related works."],
+  price:"Rs. 1000 for Proprietorship firm 1500 for Partnership/company or others",      
+  id:2,
+},
+{
+ title: "Digital Signature Service",
+ options:["Proprietorship","Private Limited","Partnership Firm","Limited Liability partnership","Government Org",
+         "NGO and Trust"],
+ docs:[],
+ work:[],
+ price:"Rs.1200",
+ id:8,
 }]
 export default Info;
--- src/components/Serviceinfo.js
+++ src/components/Serviceinfo.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Info from './Info';
 import { NavLink } from 'react-router-dom';
 import "./Icard.css";
 import { useParams } from 'react-router';
+import { useState } from 'react/cjs/react.development';
 const Serviceinfo = () => {
+    const [show,setShow] = useState(0);
     const sid = useParams();
     const sDetail = Info.filter(x=>x.id === parseInt(sid.id));
     const sd = sDetail[0];
-    console.log(sDetail);
+    useEffect(() => {
+        if(parseInt(sid.id)===1){
+            setShow(1);
+        }
+        else{
+            setShow(0);
+        }   
+    }, [])
     return (
         <div className="service_info">
             <div className="info-container">
@@ -23,6 +32,7 @@ const Serviceinfo = () => {
                                 })
                                 }
                                 <h1 className="price"><span>Price : </span>{sd.price} <span>excl.of gov tax</span></h1>
+                                <h3>Government fee Approx 1500-4000 depending upon share capital or state of registered office</h3>
                                 <NavLink  to="/contact" onClick={()=>{window.scrollTo(0,0)}}>
                                      <button className="con-link">Contact Now</button> 
                                 </NavLink>  
@@ -30,11 +40,41 @@ const Serviceinfo = () => {
                             <div className="wbox">
                                 <h1>What You Get:</h1>
                                 {
-                                    sd.docs.map((val)=>{
+                                    sd.work.map((val)=>{
                                     return <li>{val}</li>
                                 })
                                 }
                             </div>
+                            <div className="wbox">
+                                <h1>Requried Documents:</h1>
+                                {
+                                    sd.docs.map((val)=>{
+                                        return <li>{val}</li>
+                                    })
+                                }
+                            </div>
+                            {show?(
+                                <div className="wbox">
+                                    <h1>For 12A & 80G Registrations Required Documents:</h1>
+                                    <h3>All documents 3 sets:</h3>
+                                    <li>Registration Certificate and Memorandum of association, Articles of association/Trust Deed</li>
+                                    <li>NOC from Landlord (where registered office is situated), along with proof of his ownership</li>
+                                    <li>Copy of PAN card of NGO</li>
+                                    <li>Electricity Bill / House tax Receipt /Water Bill (photocopy)</li>
+                                    <li>Evidence of welfare activities carried out and Progress Report since inception or last 3 years</li>
+                                    <li>A note specifying the main area of your charitable/ religious activities and a projection/ plan for the main charitable/ religious activities to be undertaken in the next two years</li>
+                                    <li>Balance sheet, Profit and loss, ITR and Audit Report of last 3years</li>
+                                    <li>List of donors along with their address and PAN</li>
+                                    <li>List of governing body i.e. board of trustees members with their contact details</li>
+                                    <li>A certified copy of the Trust Deed/ Memorandum of Association and produce original copy for verification</li>
+                                    <li>Details of donations made since inception/ during last three years.</li>
+                                    <li>Please attach details of donations received, including corpus donation, received since inception/ during last three years.</li>
+                                    <li>Please attach details of your bank accounts including name of the bank, branch, type of account and number of account.</li>
+                                    <li>Passport size photo of each member (2 nos.)</li>
+                                    <h1 className="price"><span>Price : </span>18500 <span>excl.of gov tax</span></h1>
+                                    <h3>Government fee Approx 1500-4000 depending upon share capital or state of registered office</h3>
+                                </div>
+                            ):(<div className="no">No</div>)}
                         </div>        
                     </div>
                 </div>
