40e1c5a05ac70dfeb9d076f50bac10c1e9800b09
SagarSB <pinnintisagar546@gmail.com>
2021-12-07 14:34:37 +0530

    commit on main

--- src/components/Serviceinfo.js
+++ src/components/Serviceinfo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Info from './Info';
+import { NavLink } from 'react-router-dom';
 import "./Icard.css";
 import { useParams } from 'react-router';
 const Serviceinfo = () => {
@@ -10,31 +11,33 @@ const Serviceinfo = () => {
     return (
         <div className="service_info">
             <div className="info-container">
-        <div className="icard">
-            <div className="icard-container">
-                 <h1>{sd.title}</h1>
-                 <div className="combox">
-                 <div className="sbox">
-                 <h1>Services are:</h1>
-                 {
-                     sd.options.map((v)=>{
-                         return <li>{v}</li>
-                     })
-                 }
-                 <h1>Price:{sd.price} excl.of gov tax</h1>
-                 </div>
-                 <div className="wbox">
-                 <h1>What You Get:</h1>
-                 {
-                   sd.docs.map((val)=>{
-                    return <li>{val}</li>
-                })
-                 }
-                 </div>
-                 </div>
-                 
-            </div>
-        </div>
+                <div className="icard">
+                     <div className="icard-container">
+                        <h1 className="ttl">{sd.title}</h1>
+                        <div className="combox">
+                            <div className="sbox">
+                                <h1>Services are:</h1>
+                                {
+                                    sd.options.map((v)=>{
+                                    return <li>{v}</li>
+                                })
+                                }
+                                <h1 className="price"><span>Price : </span>{sd.price} <span>excl.of gov tax</span></h1>
+                                <NavLink  to="/contact" onClick={()=>{window.scrollTo(0,0)}}>
+                                     <button className="con-link">Contact Now</button> 
+                                </NavLink>  
+                            </div>
+                            <div className="wbox">
+                                <h1>What You Get:</h1>
+                                {
+                                    sd.docs.map((val)=>{
+                                    return <li>{val}</li>
+                                })
+                                }
+                            </div>
+                        </div>        
+                    </div>
+                </div>
             </div>   
         </div>
     )
